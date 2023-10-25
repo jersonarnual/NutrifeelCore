cf86b9ba0febb0a23abd803934dd3032ed77938f
Jerson Arnual Moreno Quevedo <jerson.moreno.out@servientrega.com>
2023-10-25 18:38:24 -0500

    Implementacion de entidades varias

--- NutrifeelCore.Domain/Common/EntityBase.cs
+++ NutrifeelCore.Domain/Common/EntityBase.cs
@@ -6,8 +6,8 @@ namespace NutrifeelCore.Domain.Common
     {
         [Key]
         public Guid Id { get; set; }
-        public string CreateBy { get; set; }
-        public string UpdateBy { get; set; }
+        public string? CreateBy { get; set; }
+        public string? UpdateBy { get; set; }
         public DateTime CreateTime { get; set; }
         public DateTime UpdateTime { get; set; }
     }
---
+++ NutrifeelCore.Domain/Domain/CurriculumPerson.cs
@@ -0,0 +1,18 @@
+﻿using NutrifeelCore.Domain.Common;
+using NutrifeelCore.Domain.Domain.MasterTable;
+
+namespace NutrifeelCore.Domain.Domain
+{
+    public class CurriculumPerson : EntityBase
+    {
+
+        public string JobTitle { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool Graduate { get; set; }
+        public Guid PersonId { get; set; }
+        public Guid LanguagesId { get; set; }
+        public virtual ICollection<Person> Persons { get; set; }
+        public virtual ICollection<Languages> Languages{ get; set; }
+    }
+}
